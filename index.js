c2e421e068061426e72c2273d882b8dfa1c5bfb2
Renukta-21 <edu211004@gmail.com>
2024-07-01 16:17:54 -0600

    Updated fetch routes

--- index.js
+++ index.js
@@ -8,29 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-const userEntries = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-]
-
 const welcomeAPI =
 `<h2>Porfavor continue con los endpoints apropiados</h2>
 <p>/contacts GET all DB items</p>
@@ -91,7 +68,7 @@ app.delete('/contacts/:id', (req, res, next) => {
     })
 })
 
-app.get('/info', (req, res) => {
+/* app.get('/info', (req, res) => {
   const fecha = new Date()
   const horas = fecha.getHours().toString().padStart(2, '0')
   const minutos = fecha.getMinutes().toString().padStart(2, '0')
@@ -99,10 +76,10 @@ app.get('/info', (req, res) => {
   const horaFormateada = `${horas}:${minutos}:${segundos}`
 
   res.send(
-        `<p>Phonebook has info for ${userEntries.length} people</p>
+        `<p>Phonebook has info for ${.length} people</p>
         <p>${new Date().toDateString()}  ${horaFormateada} </p>
         `)
-})
+}) */
 
 app.post('/contacts', (req, res, next) => {
   const regexExp = /^\d{2,3}-\d{4,6}$/g
