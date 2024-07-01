cc3088f15c8f5eeb420cf08f12ee98725df60639
Renukta-21 <edu211004@gmail.com>
2024-06-30 19:58:56 -0600

    Finished API actions and added requests templates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-let userEntries = [
+const userEntries = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -63,7 +63,7 @@ app.get('/contacts', (req, res) => {
     .catch(err => console.log('An error has ocurred: ' + err))
 })
 
-app.get('/contacts/:id', (req, res) => {
+app.get('/contacts/:id', (req, res, next) => {
   Contact.findById(req.params.id)
     .then(response => {
       if (!response) {
@@ -74,19 +74,23 @@ app.get('/contacts/:id', (req, res) => {
       console.log('Se encontro JAJAJA ' + response)
       res.send(response)
     })
+    .catch(err => {
+      next(err)
+    })
 })
 
-app.delete('/contacts/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const exixts = userEntries.find(user => user.id === id)
-
-  if (exixts) {
-    userEntries = userEntries.filter(user => user.id !== id)
-    res.send(exixts)
-  } else {
-    res.status(404).send({ error: 'User not in DB' })
-  }
+app.delete('/contacts/:id', (req, res, next) => {
+  Contact.findByIdAndDelete(req.params.id)
+    .then(response => {
+      console.log(`Response: ${response}`)
+      if (!response) return res.status(404).send({ error: 'Id not found' })
+      res.status(204).end()
+    })
+    .catch(err => {
+      next(err)
+    })
 })
+
 app.get('/info', (req, res) => {
   const fecha = new Date()
   const horas = fecha.getHours().toString().padStart(2, '0')
@@ -102,16 +106,36 @@ app.get('/info', (req, res) => {
 
 app.post('/contacts', (req, res) => {
   const newContact = new Contact({
-    name: 'Hola pendejines',
-    number: 5630561334
+    name: req.body.name,
+    number: req.body.number
   })
   newContact.save()
     .then((result) => {
       console.log('New contact added succefully: ' + result)
       res.send(result)
     })
-    .catch(err => console.log('An error has ocurred: ' + err))
+    .catch(err => {
+      console.log('An error has ocurred: ' + err)
+    })
 })
+app.put('/contacts/:id', (req, res) => {
+  const updatedContact = {
+    name: req.body.name,
+    number: req.body.number
+  }
+  Contact.findByIdAndUpdate(req.params.id, updatedContact, { new: true })
+    .then(response => {
+      console.log(response)
+      res.status(200).end()
+    })
+})
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message)
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted Id' })
+  }
+}
+app.use(errorHandler)
 
 const unknownPath = (req, res) => {
   res.status(404).send({ error: 'Path not found' })
